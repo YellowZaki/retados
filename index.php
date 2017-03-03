6da0a38f7179fd35ecad78d483fc5f38678560b2
profeIAP <jasvazquez@iesalandalus.com>
2017-03-03 20:51:21 +0100

    Se añade formulario 'demo' para probar con datos de tipo 'post'

--- index.php
+++ index.php
@@ -119,16 +119,30 @@ $app->group('/preguntas', function () use ($app) {
 		$datos=Pregunta::cargar($app->request()->get('ID'));
 		echo json_encode($datos);
 	});
+	
 	$app->post('/guardar', function() use ($app){
 		global $twig;
 		$valores=Utilidades::getDatosPreguntas($app);
 		AccesoDatos::guardar($app->db,"PREGUNTAS", $valores);
 		$app->redirect('/preguntas');
-		
 	});
-			
 });
 
+/*
+ * Este bloque irá fuera tan pronto tengamos el primer formulario operativo
+ */
+ 
+$app->group('/prueba', function () use ($app) {
+	$app->get('/', function() use ($app){
+		global $twig;
+		echo $twig->render('prueba.php');
+	});
+	$app->post('/guardar', function() use ($app){
+		global $twig;
+		$valores=Utilidades::getDatosFormulario($app);
+		echo json_encode($valores);
+	});
+});
 
 $app->get('/about', function() use ($app){
 	global $twig;
