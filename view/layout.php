b6445969e147f9a7962521fb89144a70a03315d4
usuario <algoca99@gmail.com>
2017-03-17 11:18:45 +0100

    Arreglada barra sticky

--- view/layout.php
+++ view/layout.php
@@ -200,8 +200,8 @@
 
 	<!-- script references -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
 		<script src="http://maps.googleapis.com/maps/api/js?sensor=false&extension=.js&output=embed"></script>
-		<script src="js/scripts.js"></script>
+		<script src="/js/scripts.js"></script>
 	</body>
 </html>
