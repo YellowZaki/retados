1f9e5bf80ede1d46e0e621e248a60e12ae83b50e
profeIAP <jasvazquez@iesalandalus.com>
2017-03-17 16:12:56 +0100

    Se reduce el tamaño de la cabecera web. Se restituye el nombre actualmente acordado para el proyecto.

--- view/layout.php
+++ view/layout.php
@@ -24,7 +24,7 @@
     <div class="container">
     <div class="row">
       <div class="col-sm-6">
-        <h1><a href="/" title="Bootstrap Template">Reta2</a>
+        <h1><a href="/" title="Bootstrap Template">{retA2}</a>
           <p class="lead">I.E.S Al-Ándalus</p></h1>
           
 
