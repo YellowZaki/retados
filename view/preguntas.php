92671ef5017b534e927596c3120dcb414e93d8fd
manu21499 <2010.5c.manuelg@gmail.com>
2017-04-21 11:29:05 +0200

    Implemento la tarea #41

--- view/preguntas.php
+++ view/preguntas.php
@@ -16,15 +16,33 @@
 	<h1>Preguntas registradas</h1>
 	<p class="lead">Listado de preguntas</p>
 </div>
-
+	<div class="form-group col-md-12">
+			<table class="table table-bordered table-hover" id="invoiceTable">				
+				<thead>
+					<tbody style= ".table-hover">
+					<tr>
+							
+				<th width="95%">Preguntas</th>
+				<th width="5%">Respuestas</th>
+	
+					</tr>
+					</tbody>
+				</thead>
+	</div>	
 {% for pregunta in preguntas %}
-	
-		{{pregunta.TEXTO|raw}}<BR>
+
+<div>
+	<tbody>
+<tr>
+	<td width="95%">Preguntas</td>
+	<td width="5%"><a href="/preguntas/borrar?ID={{pregunta.ID}}"><img width="32px" src="/img/borrar.png"></a>
+		<a href="/preguntas/editar?ID={{pregunta.ID}}"><img width="32px" src="/img/editar.png"></a><br></td>
 		
-		<a href="/preguntas/borrar?ID={{pregunta.ID}}"><img width="32px" src="/img/borrar.png"></a>
-		<a href="/preguntas/editar?ID={{pregunta.ID}}"><img width="32px" src="/img/editar.png"></a><br>
-		----------------<br>
+</tr>
 {% endfor %}
-	
+</tbody>
+</table>
+</div>
+
 {% endblock cuerpo %}
 
